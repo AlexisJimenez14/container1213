d97e7938250778bbfc27f93ff90d4b777cc83d7b
ALEXIS_FABIAN_JIMENEZ_JIMENEZ <a.22308051281213@cbtis128.edu.mx>
2025-03-20 18:54:19 +0000

    fin

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Row(
@@ -22,35 +21,40 @@ class MyApp extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.blue[200],
-              borderRadius: BorderRadius.circular(12.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 3,
-                  blurRadius: 7,
-                  offset: Offset(0, 3),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue[200],
+                  borderRadius: BorderRadius.circular(12.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Nombre: Alexis Jiménez',
-                      style: TextStyle(fontSize: 18)),
-                  SizedBox(height: 10),
-                  Text('Edad: 25 años', style: TextStyle(fontSize: 18)),
-                  SizedBox(height: 10),
-                  Text('Correo: alexis.jmz@example.com',
-                      style: TextStyle(fontSize: 18)),
-                ],
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Nombre: Alexis Jiménez',
+                          style: TextStyle(fontSize: 18)),
+                      SizedBox(height: 10),
+                      Text('Edad: 25 años', style: TextStyle(fontSize: 18)),
+                      SizedBox(height: 10),
+                      Text('Correo: alexis.jmz@example.com',
+                          style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                ),
               ),
-            ),
+              // Puedes agregar más widgets aquí debajo del Container
+            ],
           ),
         ),
       ),
